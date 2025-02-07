728c40d6de658a61888ad1f4315fc518b97e434e
alae-eddine sahbou <alae.shb147@gmail.com>
2025-02-07 20:13:32 +0100

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -5,7 +5,7 @@ const geminiApiKey = "AIzaSyCcEfLxskeNUABJC3RnJGWWhkrQ7R80bL0";
 export const articleApi = createApi({
   reducerPath: 'articleApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.generativeai.google.com/v1beta2',
+    baseUrl: 'https://api.generativeai.google.com/v1beta',
     prepareHeaders: (headers) => {
       headers.set('Authorization', `Bearer ${geminiApiKey}`);
       headers.set('Content-Type', 'application/json');
